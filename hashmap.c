964f68e246b0008c3730cd37cd98b7216d5ef975
allisonrobbins <32199978+allisonrobbins@users.noreply.github.com>
2018-11-20 19:35:19 -0500

    Add files via upload

---
+++ hashmap.c
@@ -0,0 +1,21 @@
+#include "hashmap.h"
+
+struct hashmap* hm_create(int num_buckets){
+
+}
+
+int hm_get(struct hashmap* hm, char* word, char* document_id){
+
+}
+
+void hm_put(struct hashmap* hm, char* word, char* document_id, int num_occurrences){
+
+}
+
+void hm_destroy(struct hashmap* hm){
+
+}
+
+int hash(struct hashmap* hm, char* word, char* document_id){
+
+}
