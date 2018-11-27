5bdfa6dc7fa81746cb2f0c5cae0530fff7907d33
Allison Robbins <allisonrobbins@gwu.edu>
2018-11-27 18:24:20 +0000

    Final code

--- test.c
+++ test.c
@@ -4,42 +4,9 @@
 #include "hashmap.h"
 
 
-void readFile2(char* input_file, struct hashmap *hm);
-void readFile3(char* input_file, struct hashmap *hm);
+void readFile(char* input_file, struct hashmap *hm);
 void hm_print(struct hashmap* hm);
-void readFile2(char* input_file,struct hashmap *hm)
-{
-  FILE *fptr;
-  int i;
-  char curr;
-  fptr = fopen(input_file, "r");
-  //printf("opened file %s\n",input_file);
-  while(curr!=EOF)
-  {
-    //printf("Starting to read a new word\n");
-    char arr[50];
-    curr = (char)fgetc(fptr);
-    int k=0;
-    while(curr!=' ' && curr!= EOF)
-    {
-      arr[k++] = curr;
-      curr = (char)fgetc(fptr);
-    }
-    arr[k] = 0;
-    hm_put(hm,arr,input_file,1);
-    for(i=0; i<k; i++)
-    {
-      printf("%c",arr[i]);
-    }
-    printf(" \n");
-    if(curr == EOF)
-    {
-      return;
-    }
-  }
-  printf("\n");
-}
-void readFile3(char* input_file,struct hashmap *hm)
+void readFile(char* input_file,struct hashmap *hm)
 {
   printf("reading file %s\n",input_file);
   FILE *fptr;
@@ -55,7 +22,7 @@ void readFile3(char* input_file,struct hashmap *hm)
   while(word!=NULL)
   {
     hm_put(hm,word,input_file,1);
-    word = strtok(line, " ");
+    word = strtok(NULL, " ");
   }
   fclose(fptr);
 }
@@ -78,8 +45,7 @@ void hm_print(struct hashmap* hm)
     }
     while(iter!=NULL)
     {
-      char* wrd = iter->word;
-      printf("Word = %s, Document ID = %s, Number of Occurrences = %i\n",wrd,iter->document_id,iter->num_occurrences);
+      printf("Word = %s, Document ID = %s, Number of Occurrences = %i\n",iter->word,iter->document_id,iter->num_occurrences);
       iter = iter->next;
     }
     printf("\n");
@@ -90,13 +56,11 @@ int main(void)
 {
   printf("___________________________________________________________\n");
   printf("Create the hashmap and input files:\n");
-  struct hashmap *hm = hm_create(17);
+  struct hashmap *hm = hm_create(23);
   printf("I made a hashmap\n");
-  readFile3("input1.txt",hm);
-  hm_print(hm);
-  /*readFile3("input2.txt",hm);
-  hm_print(hm);
-  readFile2("input3.txt",hm);
+  readFile("input1.txt",hm);
+  readFile("input2.txt",hm);
+  readFile("input3.txt",hm);
   hm_print(hm);
   printf("___________________________________________________________\n");
   printf("Test get ~you~ from input2.txt (should be 6): %d\n", hm_get(hm, "you", "input2"));
@@ -107,6 +71,6 @@ printf("___________________________________________________________\n");
 	hm_destroy(hm);
   printf("\n");
   printf("___________________________________________________________\n");
-*/
+
 }
 
